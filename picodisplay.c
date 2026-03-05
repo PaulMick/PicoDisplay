d2fbd56ff481d7534a3c4501e819f3482ccfef31
PaulMick <paulwmick@gmail.com>
2026-03-04 19:45:34 -0500

    about to try DMA

--- picodisplay.c
+++ picodisplay.c
@@ -31,17 +31,29 @@ int init() {
 }
 
 int run() {
-    uint frame_count = 0;
+    // int x = 0;
+    // int dir = 1;
     while (1) {
-        // for (int i = 0; i < 16; i ++) {
-        //     draw_rect(i, i, 64 - 2 * i, 32 - 2 * i, 1, 255 - 16 * i, 255 - 16 * i, 255 - 16 * i);
+        fill_frame(0, 0, 0);
+        ///////////////////////////
+        // if (x >= 63 && dir == 1) {
+        //     dir = -1;
+
+        // } else if (x <= 0 && dir == -1) {
+        //     dir = 1;
         // }
+
         for (int i = 0; i < 64; i ++) {
             draw_line(i, 0, LINE_DOWN, 32, 255 - 4 * i, 0, i * 4);
         }
 
+        // draw_line(x, 0, LINE_DOWN, 32, 255, 255, 255);
+
+        
+        // x += (x > 1 ? (x / 16) + 1 : 1) * dir;
+        ///////////////////////////
+        sleep_ms(1);
         update_frame();
-        frame_count ++;
-        sleep_ms(1000);
+        sleep_ms(9);
     }
 }
